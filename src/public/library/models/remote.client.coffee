joostverdoorn/webrtc

commit e71ad7fb9390c09396661d927df6d3b17a46f126

Fix bugs with splitting node class and added a mobile controller class

src/public/library/models/remote.client.coffee
@@ -14,14 +14,15 @@ define [
 		initialize: ( @_connection ) ->
 			@id = @_connection.id
 
-			@_connection.on('message', ( message ) => @trigger('message', message))
+			@_connection.on('message', ( message ) => @trigger('message', message); console.log message)
 			@_connection.on('disconnect', ( ) => @trigger('disconnect'))
 
 			@on('setSuperNode', @_onSetSuperNode)
 
+			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
 			@query('benchmark', ( benchmark ) => @benchmark = benchmark)
 			@query('system', ( system ) => @system = system)
-			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
+
 
 
 		# Disconnects from the client.

src/public/library/models/remote.peer.coffee
@@ -207,9 +207,9 @@ define [
 				)
 			, 7500)
 
+			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
 			@query('benchmark', ( benchmark ) => @benchmark = benchmark)
 			@query('system', ( system ) => @system = system)
-			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
 
 			@trigger('channel.opened', @, event)
 

src/public/library/node.coffee
@@ -59,7 +59,6 @@ define [
 			@_peers.on('disconnect', @_onPeerDisconnect)
 
 
-
 			@runBenchmark()
 			@initialize?.apply(@)
 
@@ -219,7 +218,21 @@ define [
 		# @param message [Message] the message to relay.
 		#
 		relay: ( message ) ->
-			@server.send(message)
+			peer.send(message)
+
+
+		# Responds to a request
+		#
+		# @param request [String] the string identifier of the request
+		# @param args... [Any] any arguments that may be accompanied with the request
+		# @param from [Peer] the peer we received the query from
+		# @return [Object] a response to the query
+		#
+		query: ( request, args... ) ->
+			switch request
+				when 'isStructured'
+					return @isStructured
+			return false
 
 		# Runs a benchmark to get the available resources on this node.
 		#
@@ -228,5 +241,5 @@ define [
 			endTime = Date.now()
 			@benchmark.cpu = Math.round(endTime - startTime)
 
-		_enterNetwork: () ->
+		enterNetwork: () ->
 			console.log "enternetwork method is not implemented"

src/public/library/node.structured.coffee
@@ -344,7 +344,7 @@ define [
 		#
 		_pickParent: ( candidates = null, callback ) =>
 			unless candidates?
-				candidates = _(@getPeers()).filter( ( p ) => p.isSuperNode )
+				candidates = _(@getPeers()).filter( ( p ) -> p.isSuperNode )
 			if candidates.length > 0
 				candidates = _(candidates).sortBy( 'latency' )
 				candidate = candidates.shift()
@@ -358,7 +358,7 @@ define [
 				)
 			else
 				callback?(false)
-				@_enterNetwork()
+				@enterNetwork()
 
 		# Generates a new token and gives it to a random child 
 		#
@@ -435,7 +435,6 @@ define [
 			@token.position = @coordinates.substract(tokenForce)		# Calculate the new Token Position and save it in Token object
 			tokenMagnitude = @coordinates.getDistance(@token.position)
 
-			console.log tokenMagnitude, "magnitude"
 			if (tokenMagnitude > tokenThreshhold)
 				# Ask other supernodes for their best child in neighboorhood of the tokenPosition
 				@broadcast('token.requestCandidate', @token.serialize())
@@ -489,7 +488,6 @@ define [
 			if @token?
 				bestCandidateDistance = null
 				for candidate in @token.candidates ? []
-					console.log  candidate
 					if !bestCandidateDistance? or candidate.distance < bestCandidateDistance
 						bestCandidateDistance = candidate.distance
 						bestCandidate = candidate

src/public/controller.html
@@ -1,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Controller</title>
+	<script data-main="scripts/app.controller" src="vendor/scripts/require.js"></script>
+	<link rel="stylesheet" href="vendor/stylesheets/bootstrap.css">
+</head>
+
+<body>
+	Mobile controller
+
+
+</body>
+
+</html>

src/public/library/models/remote._.coffee
@@ -41,7 +41,6 @@ define [
 		#
 		_onMessage: ( messageString ) =>
 			message = Message.deserialize(messageString)
-			
 			if message.isStored()
 				return
 

src/public/library/models/remote.server.coffee
@@ -7,6 +7,8 @@ define [
 
 	class Remote.Server extends Remote
 
+		id: 'server'
+
 		# Initializes this class. Will connect to a remote server using sockets.
 		# Is called from the baseclass' constructor.
 		#

src/public/scripts/app.controller.coffee
@@ -1,0 +1,55 @@
+requirejs.config
+	baseUrl: '../'
+
+	shim:
+		'jquery':
+			exports: '$'
+
+		'bootstrap': [ 'jquery' ]
+		'jquery.plugins': [ 'jquery' ]
+
+	# We want the following paths for 
+	# code-sharing reasons. Now it doesn't 
+	# matter from where we require a module.
+	paths:
+		'public': './'
+
+		'jquery': 'vendor/scripts/jquery'
+		'bootstrap': 'vendor/scripts/bootstrap'
+		
+require [
+	'scripts/app._'
+	'library/node'
+	'jquery'
+	], ( App, Node, $ ) ->
+
+	# Master app class
+	#
+
+	class App.Controller extends App
+		
+		# This method will be called from the baseclass when it has been constructed.
+		# 
+		initialize: ( ) ->
+			@node = new Node()
+
+			nodeId =  @getURLParameter("nodeId")
+			@node.server.on('connect', ( ) =>
+				@node.connect(nodeId)
+			)
+			@node._peers.on('channel.opened', ( ) =>
+				console.log "hoi"
+				@node.server.disconnect()
+			)
+
+
+		getURLParameter: (name) ->
+			results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href)
+			unless results?
+				return null
+			else
+				return results[1] || 0
+
+
+			
+	window.App = new App.Controller

commit d76623f932f2c93e8bd356c918dc8468c693ffa5

Fix tests that e71ad7fb9390c09396661d927df6d3b17a46f126 broke

src/public/library/models/remote.client.coffee
@@ -19,11 +19,9 @@ define [
 
 			@on('setSuperNode', @_onSetSuperNode)
 
-			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
 			@query('benchmark', ( benchmark ) => @benchmark = benchmark)
 			@query('system', ( system ) => @system = system)
-
-
+			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
 
 		# Disconnects from the client.
 		#

src/public/library/models/remote.peer.coffee
@@ -207,9 +207,9 @@ define [
 				)
 			, 7500)
 
-			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
 			@query('benchmark', ( benchmark ) => @benchmark = benchmark)
 			@query('system', ( system ) => @system = system)
+			@query('isSuperNode', ( isSuperNode ) => @isSuperNode = isSuperNode)
 
 			@trigger('channel.opened', @, event)
 

src/public/library/node.coffee
@@ -50,7 +50,7 @@ define [
 
 			@server = new Server(@, @serverAddress)
 
-			@server.on('connect', @enterNetwork)
+			@server.on('connect', @_enterNetwork)
 			@server.on('peer.connectionRequest', @_onPeerConnectionRequest)
 			@server.on('peer.setRemoteDescription', @_onPeerSetRemoteDescription)
 			@server.on('peer.addIceCandidate', @_onPeerAddIceCandidate)
@@ -241,5 +241,5 @@ define [
 			endTime = Date.now()
 			@benchmark.cpu = Math.round(endTime - startTime)
 
-		enterNetwork: () ->
-			console.log "enternetwork method is not implemented"
+		_enterNetwork: () ->
+			console.log "_enterNetwork method is not implemented"

src/public/library/node.structured.coffee
@@ -237,7 +237,7 @@ define [
 		# are found, or it connect to a bunch of other supernodes
 		# and pick the one with the lowest latency is parent.
 		#
-		enterNetwork: ( ) =>
+		_enterNetwork: ( ) =>
 			@server.query('nodes', ( nodes ) =>
 				superNodes = _(nodes).filter( ( node ) => node.isSuperNode )
 
@@ -358,7 +358,7 @@ define [
 				)
 			else
 				callback?(false)
-				@enterNetwork()
+				@_enterNetwork()
 
 		# Generates a new token and gives it to a random child 
 		#